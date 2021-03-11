acf821a1f7f063049a66f0593d8622314958f7f3
RYZEN <RYZEN@PC-RYZEN>
2021-03-11 19:15:04 +0700

    modified:   Setup_BRI_Reporting_Tool.exe 	modified:   gzip.lua

--- gzip.lua
+++ gzip.lua
@@ -5,20 +5,15 @@ function format_number(v)
 	local unary
 	
 	if v < 0 then
-		--s = string.format("%d", math.floor(-v))
 		s = tostring(-v)
 		unary = "-"
 	else
-		--s = string.format("%d", math.floor(v))
 		s = tostring(v)
 		unary = ""
 	end
-    
     local pos = string.len(s) % 3
-
     if pos == 0 then pos = 3 end
     return unary..string.sub(s, 1, pos).. string.gsub(string.sub(s, pos+1), "(...)", ".%1")
---	return v
 end
 
 -- stream the text file into a gzip file
@@ -26,8 +21,8 @@ params = {...}
 filename = params[1]
 
 t1 = os.clock()
-fi = assert(io.open(filename, "r"))
-gzFile = assert(gzio.open(filename..".gz", "w"))
+fi = assert(io.open(filename, "rb"))
+gzFile = assert(gzio.open(filename..".gz", "wb"))
 repeat
 	buf = fi:read(1024)
 	if buf ~= nil then gzFile:write(buf) end
@@ -35,4 +30,4 @@ until buf == nil
 gzFile:close()
 fi:close()
 
-iup.Message("Info", "Successfully compress "..filename.." into \n"..filename..".gz in "..(os.clock()-t1).."\nFile size reduced: "..format_number(os.getfilesize(filename)-os.getfilesize(filename..'.gz')..' bytes'))
+iup.Message("Info", "Successfully compress "..filename.." into "..filename..".gz in "..(os.clock()-t1).." sec.\nFile size reduced: "..format_number(os.getfilesize(filename)-os.getfilesize(filename..'.gz'))..' bytes')
